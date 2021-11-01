d92b698ad140921f94acd839de57903505ccdcfb
isly21 <vegitobi@hotmail.com>
2021-11-01 22:08:05 +0100

    ajout de node.js

---
+++ serveur/mongoDB.js
@@ -0,0 +1,49 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/mydb";
+
+// creer une collection
+/*MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  });*/
+
+  // inserer des valeur
+insererData = (url) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+
+
+        var myobj = [
+            { _id: 160, name: 'dodo'}
+            ];
+
+            dbo.collection("products").insertMany(myobj, function(err, res) {
+            if (err) throw err;
+            console.log(" documents inserted : "+res.insertedCount);
+            db.close();
+        });
+    });
+}
+
+rechercherTous = (bdd = "mydb", collection = "customers") => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(bdd);
+        var query = { address: /^S/ };
+        dbo.collection(collection).find(query).toArray( function(err, result) {
+        //dbo.collection("customers").find({}, { projection: { address: 0 } }).toArray(function(err, result) { // on enleve l'attribut addresse
+          if (err) throw err;
+          console.log(result);
+          db.close();
+        });
+      });
+}
+
+ // insererData(url);
+ rechercherTous();
---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import NameForm from './NameForm';
+import './index.css';
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
+
+
+
+
+//import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+
+class App extends React.Component {
+
+    render() {
+      return (
+      <div>
+         <Router>
+         <div>
+           <h2>Welcome to React Router Tutorial</h2>
+           <nav className="navbar navbar-expand-lg navbar-light bg-light">
+           <ul className="navbar-nav mr-auto">
+             <li><Link to={'/'} className="nav-link"> Home </Link></li>
+             <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+             <li><Link to={'/about'} className="nav-link">About</Link></li>
+           </ul>
+           </nav>
+           <hr />
+           <Switch>
+               <Route exact path='/' component={Home} />
+               <Route path='/contact' component={Contact} />
+               <Route path='/about' component={About} />
+           </Switch>
+         </div>
+         </Router>
+         <NameForm/>
+         
+        
+         
+      </div>
+       
+      )
+      }
+  
+}
+
+export default App;
